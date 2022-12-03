4e4722307837ce70936aec95988e17aba7c27209
gami0202 <taku.yamagami@gmail.com>
2022-12-04 01:31:38 +0900

    test

--- app.rb
+++ app.rb
@@ -8,6 +8,75 @@ def client
   }
 end
 
+# def carousel
+# {
+#   "type": "template",   # 必須
+#   "altText": "this is a carousel template",   # 必須
+#   "template": {         # 必須
+#       "type": "carousel",  # 必須
+#       "columns": [      # 必須
+#           # 1つ目のカラム
+#           {
+#             "title": "支払い操作",
+#             "text": "支払い操作です！",   # 必須
+#             "actions": [    # 必須
+#                 # 1つ目のボタン(カラム1)
+#                 {
+#                     "type": "postback",
+#                     "label": "登録",
+#                     "data": "action=chargeAdd"
+#                 },
+#                 # 2つ目のボタン(カラム1)
+#                 {
+#                     "type": "postback",
+#                     "label": "一覧",
+#                     "data": "action=botList"
+#                 },
+#                 # 3つ目のボタン(カラム1)
+#                 {
+#                     "type": "postback",
+#                     "label": "清算",
+#                     "data": "action=botCalc"
+#                 }
+#             ]
+#           }
+#     #       # 2つ目のカラム
+#     #       {
+#     #         "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+#     #         "imageBackgroundColor": "#000000",
+#     #         "title": "this is menu",
+#     #         "text": "description",   # 必須
+#     #         "defaultAction": {
+#     #             "type": "uri",
+#     #             "label": "View detail",
+#     #             "uri": "http://example.com/page/222"
+#     #         },
+#     #         "actions": [   # 必須
+#     #             # 1つ目のボタン(カラム2)
+#     #             {
+#     #                 "type": "postback",
+#     #                 "label": "Buy",
+#     #                 "data": "action=buy&itemid=222"
+#     #             },
+#     #             # 2つ目のボタン(カラム2)
+#     #             {
+#     #                 "type": "postback",
+#     #                 "label": "Add to cart",
+#     #                 "data": "action=add&itemid=222"
+#     #             },
+#     #             # 3つ目のボタン(カラム2)
+#     #             {
+#     #                 "type": "uri",
+#     #                 "label": "View detail",
+#     #                 "uri": "http://example.com/page/222"
+#     #             }
+#     #         ]
+#     #       }
+#       ]
+#   }
+# }
+# end
+
 post '/callback' do
   body = request.body.read
 
@@ -26,9 +95,17 @@ post '/callback' do
         message = {
           type: 'text',
           text: event.message['text']
+        #   text: carousel
         }
         client.reply_message(event['replyToken'], message)
       end
+    #   when Line::Bot::Event::Postback
+    #     message = {
+    #       type: 'text',
+    #       text: event.postback['action']
+    #     }
+    #     client.reply_message(event['replyToken'], message)
+    #   end
     end
   end
 
